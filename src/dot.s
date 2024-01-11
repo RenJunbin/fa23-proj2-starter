390a961ccb7462a997951a64426d9c7c3cb08077
renjunbin <renjunbin123@126.com>
2024-01-11 18:54:10 +0800

    modified:   src/dot.s

--- src/dot.s
+++ src/dot.s
@@ -18,26 +18,49 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
-
+    ebreak
     # Prologue
-
-
+		li t0, 1
+		bge a2, t0, c0
+		li a0, 36
+		j exit
+c0:
+		blt a3, t0, c2
+		blt a4, t0, c2
+		j init
+c2:
+		li a0, 37
+		j exit
+init:
+		li t0, -1
+		mv t1, zero
+		mv t2, zero
+		mv t3, zero
+		mv t4, zero
 loop_start:
+		addi t0, t0, 1				# t0 is loop index, t0 is 0 initially.
+		bge t0, a2, loop_end		# if (t0 >= a2) goto loop_end
+loop_continue:
+	# load a0[t0 * a3 * 4] and a1[t0 * a4 * 4] to t1, t2
+		mul t1, t0, a3
+		slli t1, t1, 2
+		mul t2, t0, a4
+		slli t2, t2, 2
 
+		add a0, a0, t1
+		add a1, a1, t2
+		lw t1, 0(a0)
+		lw t2, 0(a1)
 
+	# t3 = t1 * t2
+		mul t3, t1, t2
+	# t4 = t4 + t3
+		add t4, t4, t3
 
-
-
-
-
-
-
-
-
-
+		j loop_start
 loop_end:
 
-
+	mv a0, t4
     # Epilogue
 
 
